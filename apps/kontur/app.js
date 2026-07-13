f74067f434738130ac0314ba81bdbcf133a95f5e
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2026-07-13 20:19:02 +0100

    Подключить двухпозиционные переключатели КОНТУРА

--- apps/kontur/app.js
+++ apps/kontur/app.js
@@ -10,7 +10,7 @@
  *   cachePrefix: 'kontur-'
  * });
  */
-const PARTS = 10;
+const PARTS = 11;
 const paths = Array.from({ length: PARTS }, (_, index) => `./runtime/part-${String(index).padStart(2, '0')}.txt`);
 
 try {
