6194822e3b0def2083b55fd635f0c6f8accd9120
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2026-07-14 01:06:18 +0100

    Update ШПИЛЬКА workshop for 2.4

--- apps/shpilka/workshop.js
+++ apps/shpilka/workshop.js
@@ -2,13 +2,14 @@ import { createWorkshopMode } from '../../shared/workshop-mode.js';
 
 const workshop = createWorkshopMode({
   appName: 'ШПИЛЬКА',
-  version: '2.3.0',
+  version: '2.4.0',
   storageNamespace: 'pocket-works:shpilka',
   cachePrefix: 'shpilka-',
   onReset() {
     localStorage.removeItem('pocket-works:shpilka:state:v1');
     localStorage.removeItem('pocket-works:shpilka:state:v2');
     localStorage.removeItem('pocket-works:shpilka:prefs:v1');
+    localStorage.removeItem('pocket-works:shpilka:geometry:v24');
     window.location.reload();
   }
 });
