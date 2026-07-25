6a687c6f2e26046ad09a92e76ad3af54f5f392bd
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2026-07-25 23:20:28 +0100

    Bump Clada Workshop version

--- apps/clada/workshop.js
+++ apps/clada/workshop.js
@@ -4,7 +4,7 @@ const STORAGE_NAMESPACE = 'pocket-works:clada';
 
 createWorkshopMode({
   appName: 'КЛАДА',
-  version: '3.0.1',
+  version: '3.1.0',
   cachePrefix: 'clada-',
   storageNamespace: STORAGE_NAMESPACE,
   onReset: async () => {
