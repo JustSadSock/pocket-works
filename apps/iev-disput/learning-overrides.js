d5a6362b92a145968f45a56c1e666cb932dcf840
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2026-08-29 21:59:00 +0100

    Remove redundant quiz override file

--- apps/iev-disput/learning-overrides.js
+++
@@ -1,5 +0,0 @@
-export const UNIT_QUIZ_OVERRIDES={
-  'classical-transition':['t23','t24','t25'],
-  'german-history':['t46','t47','t49','t50','t51','t52','t53','t54'],
-  'transactions':['t66','t67','t68']
-};
