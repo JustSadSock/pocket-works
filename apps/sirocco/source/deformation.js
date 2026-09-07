fa46ad1425895e99ca625c8ff88729deb6e90b53
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2026-09-07 22:52:31 +0100

    Fix footprint mesh winding

--- apps/sirocco/source/deformation.js
+++ apps/sirocco/source/deformation.js
@@ -43,7 +43,7 @@ function makeFootprintTemplate(scene) {
       const b = a + 1;
       const d = a + row;
       const e = d + 1;
-      indices.push(a, d, b, b, d, e);
+      indices.push(b, e, d, a, b, d);
     }
   }
   VertexData.ComputeNormals(positions, indices, normals);
